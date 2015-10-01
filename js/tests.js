49f98be5f0b2c8d2a49764115db94ffeae31bbe7
Kristine Erickson <ericksonk09@gmail.com>
2015-09-30 21:32:51 -0400

    working on writing test to move pieces

--- js/tests.js
+++ js/tests.js
@@ -1,15 +1,51 @@
-var test = ('mocha').it,
- expect = ('chai').expect;
+(function(globals){ // IIFE
+ // Not this again...
 
-console.log(game.tracer());
+   // it('should totally fail', function(){
+   //   chai.expect(false).equals(true);
+   // })
 
-//  // What kind of thing is game.board...? What does it do?
- // test('initializing chess game' function() {
- //    expect(game.board).to.be.an.Array;
- // }
-//
-//  // How about game.reset? What does it return? Any side-effects?
-//
-//  // Resetting a board with no moves is lame, HOW U EVEN game.applyMove?
-//
-//  // First move is free, next one costs $10.
+   it('should have a `game` in the `window`', function(){
+     chai.expect(window.game).to.be.equal(game);
+     chai.expect(game).to.be.an('object');
+     chai.expect(game.board).to.a('function'); // ???
+   });
+
+   it('should give me a big array as the board', function(){
+     var board = game.board();
+
+     chai.expect(board).to.be.an('array');
+     // chai.expect(game.board().length).to.be.equal(8);
+     chai.expect(board).to.have.length(8);
+
+     chai.expect(board[0]).to.be.a('array');
+     chai.expect(board[0]).to.have.length(8);
+
+     chai.expect(board[0][0]).to.be.a('string');
+     chai.expect(board[0][0]).to.equal('R');
+
+   }); // END it should be a big Array
+
+   it('should be able to move pieces', function(){
+     // board[0][0] = board[0][1];
+     game.applyMove(
+       // TODO: one day take { file: 'd', rank: 2 },
+       { rank: 6, file: 3 }, // from
+       // TODO: one day take { file: 'd', rank: 4 }
+       { rank: 4, file: 3 } // to
+     );
+
+     var board = game.board();
+
+     chai.expect(board[6][4]).to.be.equal(null);
+     chai.expect(board[4][3]).to.be.equal('p');
+
+   });
+
+   it.skip('should be able to advance to the next move', function(){
+    //  chai.expect(board[0][0]).to.be.a('string');
+    //  chai.expect(board[0][0]).to.equal('p');
+   });
+
+ // There's that crazy line again...
+ })(window || module && module.exports || this);
